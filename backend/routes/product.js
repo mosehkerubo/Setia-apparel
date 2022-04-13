12e8de78b1f6123ecc3e1db667094a69bf6b753d
mosehkerubo <mosehkerubo@gmail.com>
2022-04-13 11:54:28 +0300

    modified all users and all products backend

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,11 @@
 const { route }=require("./auth");
 const product = require("../models/product");
+// const Product = require("../models/product")
+const { compareSync } = require("bcrypt");
 const router = require("express").Router();
+const { verifyTokenAndAdmin,verifyTokenAndAuthorization } = require("./verifyToken");
+
+
 
 router.get("/producttest",(req,res)=>{
     res.send("productroute")
@@ -30,6 +35,49 @@ catch(err){
 })
 
 //update
-// router.put("")
+router.put("/:id",async (req,res)=>{
+    try{
+    const id = req.params.id
+    const updates=req.body
+    const options={new:true}
+    const updatedProduct=await product.findByIdAndUpdate(id,updates,options)
+    console.log(updatedProduct)
+    res.status(200).json(updatedProduct)
+    }
+    catch(err){
+        
+    res.status(500).json(err)
+    }
+    })
+
+    //delete
+    /
+    router.delete("/:id",async (req, res) => {
+        try {
+          res.status(200).json("product succesfully deleted");
+          return await product.findByIdAndDelete(req.params.id);
+        } catch (err) {
+          res.status(404).json("record not found");
+        }
+      });
+
+
+
+// find all products
+
+router.get("/",verifyTokenAndAdmin,async(req,res)=>{
+    
+
+    try{
+        const products=await product.find()
+        // console.log(products)
+        res.status(200).json(products)
+    }
+    catch(err){
+res.json(err)
+    }
+})
+
+
 
 module.exports=router;
